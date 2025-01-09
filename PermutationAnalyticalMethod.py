7b46e9fcd661da1fa3a5f769ce46958f84a9d897
Sahand The Coder <sahand@google.com>
2025-01-09 23:04:43 +0330

    Permutation Method pdf added, the base code is written

--- PermutationAnalyticalMethod.py
+++ PermutationAnalyticalMethod.py
@@ -4,17 +4,12 @@
 """
 
 import pandas as pd
+import numpy as np
+from math import pow, factorial
+from itertools import permutations
 
 
 class PermutationMethod:
-    __decision_matrix: pd.DataFrame = None
-    __normalized_matrix: pd.DataFrame = None
-    __concordance_matrices: list = None
-
-    @staticmethod
-    def get_decision_matrix() -> pd.DataFrame:
-        """Public method to access the static property"""
-        return PermutationMethod.__decision_matrix
 
     @staticmethod
     def get_normalized_matrix() -> pd.DataFrame:
@@ -26,6 +21,61 @@ class PermutationMethod:
         """Public method to access the static property"""
         return PermutationMethod.__concordance_matrices
 
-    def __init__(self):
+    def __init__(self, normalized_matrix: pd.DataFrame, weights: list):
         # The D Matrix should be created here
+        self.normalized_matrix = normalized_matrix
+        self.weights = weights
+        # self.__concordance_matrices = self.calculate_concordance_matrix()
+
+    def calculate_concordance_matrix(self, permutated_options) -> pd.DataFrame:
+        alternatives_num = self.normalized_matrix.shape[0]
+        self.__concordance_matrices = pd.DataFrame(
+            np.zeros((alternatives_num, alternatives_num)),
+            index=self.normalized_matrix.index,
+            columns=permutated_options,
+        )
+        print(self.__concordance_matrices)
+        # for i in range(alternatives_num):
+        #     for j in range(alternatives_num):
+        #         if i != j:
+        #             self.__concordance_matrices.iloc[i, j] = (
+        #                 self.__normalized_matrix.iloc[i]
+        #                 >= self.__normalized_matrix.iloc[j]
+        #             ).sum()
+
+    def calculate_net_score(self, permutation):
         pass
+
+    def run_calculations(self):
+        options = self.normalized_matrix.index.tolist()
+        permutation_scores = {}
+        # Permutation
+        permutations_list = list(permutations(options))
+        for permutation in permutations_list:
+            print(len(permutation_scores.keys()))
+            if tuple(permutation) not in permutation_scores:
+                permutation_scores[tuple(permutation)] = 0
+                print(permutation)
+                # self.calculate_concordance_matrix(
+                #     permutated_options=current_permutation
+                # )
+                # self.calculate_concordance_matrix()
+                # score
+                # adding the score to the dict with this key
+
+        # for each permutation there should be a C Matrix and a score
+        # There must a dict of scores
+        # find the highest score
+
+
+if __name__ == "__main__":
+    data = {
+        "Criterion1": [0.8, 0.6, 0.7],
+        "Criterion2": [0.5, 0.9, 0.3],
+        "Criterion3": [0.6, 0.4, 0.8],
+    }
+    normalized_matrix = pd.DataFrame(data, index=["A1", "A2", "A3"])
+    print("Normalized Decision Matrix:")
+    print(normalized_matrix)
+    pm = PermutationMethod(normalized_matrix=normalized_matrix, weights=[0.2, 0.3, 0.5])
+    pm.run_calculations()
