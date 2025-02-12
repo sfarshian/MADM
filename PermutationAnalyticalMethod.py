45fbbd505d674569da42e1e26214fdced3944f49
Sahand The Coder <sahand@google.com>
2025-02-12 16:34:59 +0330

    Permutation Method Code Done! gitignore file added!

--- PermutationAnalyticalMethod.py
+++ PermutationAnalyticalMethod.py
@@ -1,108 +1,136 @@
 """
-    Based on what we have done in the Normalization.py file ,
-    now we can have that normalizations and use in the process of these analytical method
+    Building on the work done in the Normalization.py file, 
+    we can now apply the normalizations in the process of these analytical methods.
 """
 
 import pandas as pd
 import numpy as np
-from math import pow, factorial
 from itertools import permutations
 
 
-# TODO There is sth wrong with the concordance matrix creation, run then solve it
-
 class PermutationMethod:
+    """
+    Class to perform permutation-based method for calculating concordance matrices
+    and net scores based on a normalized decision matrix and weights.
+    """
+
     def __init__(self, normalized_matrix: pd.DataFrame, weights: list):
-        # The D Matrix should be created here
+        """
+        Initialize the PermutationMethod with the normalized matrix and weights.
+
+        Args:
+            normalized_matrix (pd.DataFrame): The normalized decision matrix.
+            weights (list): A list of weights for each attribute.
+        """
         self.normalized_matrix = normalized_matrix
         self.weights = weights
-        self.concoradance_matrices = []
-        # self.__concordance_matrices = self.calculate_concordance_matrix()
+        self.concordance_matrices = {}
+        self.permutation_scores = {}
 
-    def calculate_concordance_matrix(self, permutated_options) -> pd.DataFrame:
-        # print("*" * 80, f"\nConcordance Matrix is being Created:")
+    def calculate_concordance_matrix(self, permuted_alternatives: tuple) -> pd.DataFrame:
+        """
+        Calculate the concordance matrix for a given permutation of alternatives.
+
+        Args:
+            permuted_alternatives (list): A list of permuted alternatives.
+
+        Returns:
+            pd.DataFrame: The concordance matrix.
+        """
         alternatives_num = self.normalized_matrix.shape[0]
         returning_concordance_matrix = pd.DataFrame(
             np.zeros((alternatives_num, alternatives_num)),
-            index=permutated_options,
-            columns=permutated_options,
+            index=permuted_alternatives,
+            columns=permuted_alternatives,
         )
-        flag = False
-        if permutated_options == (0, 2, 3, 1):
-            flag = True
-        else:
-            flag = False
-        # print(returning_concordance_matrix)
-        for i in range(len(permutated_options)):
-            for j in range(len(permutated_options)):
+        for i in permuted_alternatives:
+            for j in permuted_alternatives:
                 if i != j:
-                    weight_sum = 0
-                    for attr in self.normalized_matrix.columns:
-                        if (
-                            self.normalized_matrix.iloc[permutated_options[i]][attr]
-                            >= self.normalized_matrix.iloc[permutated_options[j]][attr]
-                        ):
-                            if flag:
-                                print(f"Weight Sum {weight_sum}")
-                                print(
-                                    f"The row {permutated_options[i]} is better than row {permutated_options[j]} in {attr}"
-                                )
-                            weight_sum += self.weights[
-                                self.normalized_matrix.columns.get_loc(attr)
-                            ]
-                    returning_concordance_matrix.iloc[i, j] = weight_sum
-                    if flag:
-                        print(f"adding {weight_sum} to index {i,permutated_options[j]}")
-                        print(
-                            f"So the Updated C Matrix is \n{returning_concordance_matrix}"
-                        )
+                    self.update_concordance_matrix_cell(
+                        matrix=returning_concordance_matrix,
+                        i=i,
+                        j=j,
+                    )
         return returning_concordance_matrix
 
-    def calculate_net_score(self, permutation):
-        pass
+    def update_concordance_matrix_cell(self, matrix: pd.DataFrame, i, j):
+        """
+        Update a cell in the concordance matrix based on the comparison of two alternatives.
+
+        Args:
+            matrix (pd.DataFrame): The concordance matrix to be updated.
+            i (str): The first alternative.
+            j (str): The second alternative.
+        """
+        weight_sum = 0
+        for attr in self.normalized_matrix.columns:
+            if (
+                self.normalized_matrix.loc[i, attr]
+                >= self.normalized_matrix.loc[j, attr]
+            ):
+                weight_sum += self.weights[self.normalized_matrix.columns.get_loc(attr)]
+        matrix.loc[i, j] = round(weight_sum, 2)
+
+    def calculate_permutation_score(self, concordance_matrix: pd.DataFrame) -> float:
+        """
+        Calculate this permutation score, which is the difference between the sums of the upper and lower triangles of the concordance matrix.
+
+        Args:
+            concordance_matrix (pd.DataFrame): The concordance matrix of the corresponding permutation.
+
+        Returns:
+            float: The score.
+        """
+        upper_triangle_sum = (
+            concordance_matrix.where(
+                np.triu(np.ones(concordance_matrix.shape), k=1).astype(bool)
+            )
+            .sum()
+            .sum()
+        )
+        lower_triangle_sum = (
+            concordance_matrix.where(
+                np.tril(np.ones(concordance_matrix.shape), k=-1).astype(bool)
+            )
+            .sum()
+            .sum()
+        )
+        return round(upper_triangle_sum - lower_triangle_sum, 2)
 
     def run_calculations(self):
+        """
+        Run the permutation method calculations and save results to an output file.
+        """
         options = self.normalized_matrix.index.tolist()
-        permutation_scores = {}
-        # Permutation
+        # Permutations
         permutations_list = list(permutations(options))
         for permutation in permutations_list:
-            # print(len(permutation_scores.keys()))
-            if permutation not in permutation_scores:
-                if permutation == (0, 2, 3, 1):
-                    print(permutation)
-                    self.concoradance_matrices.append(
-                        self.calculate_concordance_matrix(permutation)
+            if permutation not in self.permutation_scores:
+                current_C_matrix = self.calculate_concordance_matrix(permutation)
+                self.concordance_matrices[tuple(permutation)] = current_C_matrix
+                self.permutation_scores[tuple(permutation)] = (
+                    self.calculate_permutation_score(
+                        concordance_matrix=current_C_matrix
                     )
-                    print(self.concoradance_matrices[-1])
-                    continue
-
-                # print(f"Permuation ({permutation}) is being scored")
-                # if(permuation ==
-                self.concoradance_matrices.append(
-                    self.calculate_concordance_matrix(permutation)
                 )
-                # print(self.concoradance_matrices[-1])
-                permutation_scores[tuple(permutation)] = 0
-                # self.calculate_concordance_matrix(
-                #     permutated_options=current_permutation
-                # )
-                # self.calculate_concordance_matrix()
-                # score
-                # adding the score to the dict with this key
+        self.write_output_file()
+
+    def write_output_file(self):
+        """
+        Write the concordance matrices and scores for each permutation to an output file.
+        
+         The final line of the output contains the best permutation and its corresponding score.
+        """
         with open("output.txt", "w") as file:
-            for m in self.concoradance_matrices:
-                file.write(m.to_string())
-                file.write("\n\n")
-                file.write("*" * 10)
-                file.write("\n\n")
-        # for each permutation there should be a C Matrix and a score
-        # There must a dict of scores
-        # find the highest score
+            for permutation, concordance_matrix in self.concordance_matrices.items():
+                file.write(
+                    f"""Permutation:\t{permutation:}\n{concordance_matrix.to_string()}\nScore:\t{self.permutation_scores.get(permutation)}\n{"*" * 50}\n"""
+                )
 
 
 if __name__ == "__main__":
     N = pd.read_csv("D_matrix.csv", skiprows=1)
+    N.index = [f"A{i+1}" for i in range(len(N))]
     W = pd.read_csv("D_matrix.csv", nrows=1, header=None).iloc[0].tolist()
     N = N.drop("OP", axis=1)
     print("Normalized Decision Matrix:")
